202a688d6668d7d6cdd1f64289db9bd96aa894b0
joacolg <joaquinlopezguerra86@gmail.com>
2025-02-24 16:08:15 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Joaquín López Guerra's blog</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <header class="d-flex justify-content-between align-items-center border-bottom p-3 custom-header">
+    <h1 class="m-0">Joaquín López Guerra</h1>
+    <nav class="d-flex gap-2">
+      <a href="#sobre-mi" class="btn text-decoration-none">boton dark mode</a>
+      <a href="html/about.html" class="btn text-decoration-none">about</a>
+      <a href="#contacto" class="btn text-decoration-none">follow me</a>
+    </nav>
+  </header>
+  
+
+  <main class="container my-5">
+    <section>
+      <ul class="list-unstyled custom-ul">
+        <section>
+            <li>date</li>
+            <li>tittle</li>
+            <li>view</li>
+        </section>
+        <li>SQL injection</li>
+        <li>Osint</li>
+        <li></li>
+        <li></li>
+      </ul>
+    </section>      
+  </main>
+
+  <footer class="text-center py-3 border-top">
+    <p class="m-0">&copy; Joaquín López Guerra - 2025</p>
+  </footer>
+
+  <!-- Bootstrap JS (opcional, si necesitas funcionalidades de Bootstrap) -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
